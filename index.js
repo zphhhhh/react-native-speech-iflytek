24289fdfd03e9f2e026a53ae47dd089cdf1a2592
zphhhhh <zphhhhh@gmail.com>
2017-08-06 10:14:17 +0800

    bugfix-SpeechConstant

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import React, { NativeModules } from "react-native";
 
 module.exports = {
+  SpeechConstant: NativeModules.SpeechConstantModule,
   Recognizer: NativeModules.SpeechRecognizerModule,
   Synthesizer: NativeModules.SpeechSynthesizerModule
 };
